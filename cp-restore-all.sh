18d1a291488c9a708bfbb1bfb418d59674b94f64
ertqrl61 <43776613+ertqrl61@users.noreply.github.com>
2024-03-24 05:12:52 +0300

    up

--- cp-restore-all.sh
+++ cp-restore-all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo -e "Where is your backup files located?"
 read backupfolder
-for backupfile in $backupfolder/*.tar; do
+for backupfile in $backupfolder/*.{tar,tar.gz}; do
     echo -e "$backupfile"
 done
