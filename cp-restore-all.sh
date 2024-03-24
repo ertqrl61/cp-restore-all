f9d71f599c6d538ed97e26299a912176ce305492
ertqrl61 <43776613+ertqrl61@users.noreply.github.com>
2024-03-24 05:19:51 +0300

    Update cp-restore-all.sh

--- cp-restore-all.sh
+++ cp-restore-all.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 echo -e "Where is your backup files located?"
 read backupfolder
+echo -e "Backup files in $backupfolder is starting to restore..."
 for backupfile in $backupfolder/*.{tar,tar.gz}; do
     echo -e "$backupfile restoring..."
     /scripts/restorepkg $backupfile
